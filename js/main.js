8ad8c3c4e4a46ece9df66fc74f33ed40c8ccd128
Kacper <kacpercybulski@icloud.com>
2024-07-29 19:05:30 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-const navBtn = document.querySelector('.hamburger') // Przycisk hamburgera
-const navMenu = document.querySelector('#nav-menu') // Nawigacja
-const body = document.body // Ciało dokumentu
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
-	// Dodaj lub usuń klasy do przycisku i menu
 	navBtn.classList.toggle('is-active')
-	navMenu.classList.toggle('nav--mobile')
-	navMenu.classList.toggle('nav--active')
-
-	// Zablokuj przewijanie, gdy menu jest aktywne
-	if (navMenu.classList.contains('nav--active')) {
-		body.style.overflow = 'hidden'
-	} else {
-		body.style.overflow = 'auto'
-	}
+	navMobile.classList.toggle('nav-mobile--active')
 }
 
-// Dodaj nasłuchiwanie kliknięcia na przycisk hamburgera
 navBtn.addEventListener('click', handleNav)
