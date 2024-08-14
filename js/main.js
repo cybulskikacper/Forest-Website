e54a365fc253bdaf872b6f95a7035999ed837cd8
Kacper <kacpercybulski@icloud.com>
2024-08-14 23:23:36 +0200

    fixed nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
+const navItems = document.querySelectorAll('.nav__item')
 const footerYear = document.querySelector('.footer__year')
+const body = document.body
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+
+	if (navMobile.classList.contains('nav-mobile--active')) {
+		body.style.overflow = 'hidden'
+	} else {
+		body.style.overflow = 'auto'
+	}
 }
 
 const handleCurrentYear = () => {
@@ -14,3 +22,11 @@ const handleCurrentYear = () => {
 handleCurrentYear()
 
 navBtn.addEventListener('click', handleNav)
+
+navItems.forEach(item => {
+	item.addEventListener('click', () => {
+		if (navMobile.classList.contains('nav-mobile--active')) {
+			handleNav() // Zamknij nawigację
+		}
+	})
+})
