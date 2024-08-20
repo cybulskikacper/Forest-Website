89d3d9fd1603db23fbd5e9028d359868583a2edb
Kacper <kacpercybulski@icloud.com>
2024-08-20 23:22:08 +0200

    .

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,15 @@
-const form = document.querySelector('.contact__form')
-const successMessage = document.querySelector('.contact__form-success')
+document.addEventListener('DOMContentLoaded', () => {
+	const form = document.querySelector('.contact__form')
+	const successMessage = document.querySelector('.contact__form-success')
 
+	const handleFormSubmit = e => {
+		e.preventDefault()
+		form.reset()
+		successMessage.style.display = 'block'
+		setTimeout(() => {
+			successMessage.style.display = 'none'
+		}, 5000)
+	}
 
-const handleFormSubmit = e => {
-	e.preventDefault()
-	form.reset()
-	successMessage.style.display = 'block'
-	setTimeout(() => {
-		successMessage.style.display = 'none'
-	}, 5000)
-}
-
-form.addEventListener('submit', handleFormSubmit)
-
+	form.addEventListener('submit', handleFormSubmit)
+})
